b34f32e17bb10160ad31f7bc4e8d523c072ab80b
VanKLuu <luukhanhvan@gmail.com>
2021-09-21 21:37:45 -0700

    updated

--- Assignment3/main.cpp
+++ Assignment3/main.cpp
@@ -7,6 +7,8 @@
 #include <iostream>
 #include <string>
 #include "input.h"
+#include "optionTwo.h"
+#include "Tower.h"
 
 void mainMenu(void);
 void programOne(void);
@@ -71,21 +73,67 @@ void programOne(void)
 //PostCondition:
 void programTwo(void)
 {
-    do
-    {
-        clearScreen();
-        /*display here*/
+	clearScreen();
+	cout << "2> Tower of Hanoi" << endl;
+	cout << string(100, char(196)) << endl;
+	cout << "The Tower of Hanoi also called the Tower of Brahma or Lucas' Tower is a mathematical game." << endl;
+	cout << "It consists of three pegs and a number of rings of different sizes, which can slide onto" << endl;
+	cout << "any peg.The game starts with the rings in a neat stack in ascending order of size on one" << endl;
+	cout << "peg, the smallest at the top, thus making a conical shape." << endl;
+	cout << "\nThe objective of the game is to move the entire stack from the starting peg - A to ending peg - B," << endl;
+	cout << "obeying the following simple rules :" << endl;
+	cout << "\n\t1. Only one disk can be moved at a time." << endl;
+	cout << "\t2. Each move consists of taking the upper disk from one of the stacks and" << endl;
+	cout << "\t   placing it on top of another stack or on an empty peg." << endl;
+	cout << "\t3. No larger disk may be placed on top of a smaller disk." << endl;
+	do
+	{
+		int numberOfRings = inputInteger("\n\t Enter the number of rings (1..64) to begin: ", 1, 64);
+		if (numberOfRings >= 10)
+			cout << "\n\tNote: The rings will be represented with numbers (1 is the smallest size and " << numberOfRings << " is the largest size)." << endl;
+		int count = 0;
+		Tower pegA, pegB, pegC;
+		for (int i = numberOfRings; i >= 1; i--)
+			pegA.push(i);
+		display(pegA, pegB, pegC, numberOfRings);
+		do
+		{
+			char source, destination;
 
-        int option = inputInteger("\t\tOption: ");
-        switch (option)
-        {
-        case '0': return;
-            /*stuff here*/
-        default: cout << "\t\tERROR-1A: Invalid input. Must be" << endl;
-            pause("\n\t\tPress enter to continue...");
-        }
+			while (true)
+			{
+				source = inputChar("\tSelect the top disk from the start peg (A, B, C, or Q - quit) : ");
+				if (source == 'A' || source == 'B' || source == 'C')
+					break;
+				else
+				{
+					if (source == 'Q')
+						return;
+					else
+						cout << "t\tERROR-1A: Invalid input. Must be 'A','B','C', or 'Q'" << endl;
+				}
+			}
 
-    } while (true);
+			while (true)
+			{
+				destination = inputChar("\tSelect the end peg (A, B, C or Q-quit) to move the selected disk: ");
+				if (destination == 'A' || destination == 'B' || destination == 'C')
+					break;
+				else
+				{
+					if (destination == 'Q')
+						return;
+					else
+						cout << "t\tERROR-1A: Invalid input. Must be 'A','B','C', or 'Q'" << endl;
+				}
+			}
+			towerOfHanoi(numberOfRings, pegA, pegB, pegC, source, destination);
+			display(pegA, pegB, pegC, numberOfRings);
+			count++;
+		} while (!pegA.isEmpty() || !pegB.isEmpty());
+		cout << "\n\tCongratulation! You have solved the game in " << count << " move." << endl;
+	} while (isRepeat("\tPlay again? (Y-yes or N-no)? "));
+
 }
 //PreCondition: 
 //PostCondition:
--- Assignment3/optionTwo.h
+++ Assignment3/optionTwo.h
@@ -1,6 +1,94 @@
-// optionTwo.h
+#include "input.h"
+#include "Tower.h"
 
-#ifndef OPTION_TWO_LOCK
-#define OPTION_TWO_LOCK
+void movedDicks(Tower &source, Tower &destination, char s, char d)
+{
+	int topSource, topDestination;
+	if (source.isEmpty())
+	{
+		cout << "\n\tERROR: Cannot make the move. There is no disk in the selected peg-" << s << endl;
+		cout << "\t       Please chooser again." << endl;
+	}
+	else
+	{
+		if (destination.isEmpty())
+		{
+			source.pop(topSource);
+			destination.push(topSource);
+			cout << "\n\tTop disk from peg-" << s << " has moved to peg -" << d << endl;
+		}
+		else
+		{
+			source.pop(topSource);
+			destination.pop(topDestination);
+			if (topSource > topDestination)
+			{
+				source.push(topSource);
+				destination.push(topDestination);
+				cout << "\n\tERROR: Cannot make the move. Top disk (" << topSource << ") of peg - " << s
+					<< " , is larger than top disk(" << topDestination << ") of peg - " << d << endl;
+				cout << "\t	    Please choose again." << endl;
+			}
+			else
+			{
+				destination.push(topDestination);
+				destination.push(topSource);
+				cout << "\n\tTop disk from peg-" << s << " has moved to peg -" << d << endl;
+			}
+		}
+	}
 
-#endif
+}
+
+void towerOfHanoi(int numberOfRings, Tower &A, Tower &B, Tower &C, char source, char destination)
+{
+
+	
+	if (source == destination)
+	{
+		cout << "\n\tERROR: Cannot make the move. The selected end peg cannot be the same as the selected start peg." << endl;
+		cout << "	    Please choose again." << endl;
+	}
+	else
+	{
+
+		if (source == 'A')
+		{
+			if(destination =='B')
+				movedDicks(A, B, source, destination);
+			else
+				movedDicks(A, C, source, destination);
+		}
+		else
+		{
+			if (source == 'B')
+			{
+				if (destination == 'A')
+					movedDicks(B, A, source, destination);
+				else
+					movedDicks(B, C, source, destination);
+			}
+			else
+			{
+				if (destination == 'A')
+					movedDicks(C, A, source, destination);
+				else
+					movedDicks(C, B, source, destination);
+			}
+		}
+	}
+}
+void display(Tower A, Tower B, Tower C, int numberOfRings)
+{
+	
+	if (numberOfRings > 9)
+	{
+		
+		cout << "\n\tTower of Hanoi" << endl;
+
+	}
+	else
+	{
+		cout << "\n\tTower of Hanoi" << endl;
+	}
+}
